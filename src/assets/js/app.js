ddee2d471e7bde5a63265dd630686f6e2370e73f
Jelle Overbeek <j.overbeek@recognize.nl>
2018-04-19 14:19:56 +0200

    added spotify OAuth

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -1,3 +1,14 @@
 (function () {
-    // Code
+    // const api = {
+    //     uri: "https://api.spotify.com/v1/me/playlists",
+    //     getData: function() {
+    //         fetch(this.uri).then(function(response) {
+    //             console.log(response);
+    //         }).catch(function(err) {
+    //             console.log('Fetch Error', err);
+    //         });
+    //     }
+    // };
+    //
+    // api.getData();
 })();
