5fcb62e624dbcd84d8a01d2c47627ee21fefd5ac
Jelle Overbeek <j.overbeek@recognize.nl>
2018-04-25 21:32:31 +0200

    added file upload

--- server.js
+++ server.js
@@ -1,10 +1,13 @@
 require('dotenv').config();
 
-const express = require('express');
-const app = express();
-const http = require('http').Server(app);
-const nunjucks = require('nunjucks');
-const io = require('socket.io')(http);
+const express = require('express'),
+    app = express(),
+    http = require('http').Server(app),
+    nunjucks = require('nunjucks'),
+    io = require('socket.io')(http),
+    formidable = require('formidable'),
+    bodyParser = require('body-parser'),
+    fs =require('fs-extra');
 
 let port = process.env.PORT || 8888;
 
@@ -15,6 +18,11 @@ nunjucks.configure('src/views', {
 });
 
 app.use(express.static(__dirname + '/src/assets'));
+
+/* ==========================================================
+ bodyParser() required to allow Express to see the uploaded files
+============================================================ */
+app.use(bodyParser({defer: true}));
 
 const times = {
     getServerTime: function () {
@@ -63,6 +71,10 @@ io.on('connection', function(socket){
         io.emit('play', playData);
     });
 
+    socket.on('uploaded', function(playData){
+        io.emit('play', playData);
+    });
+
     socket.on('userPaused', function(){
         io.emit('pause');
     });
@@ -72,6 +84,40 @@ app.get('/', function(req, res) {
     res.render('index.html', {
 
     })
+});
+
+app.get('/upload-form', function(req, res) {
+    res.render('upload-form.html', {
+
+    })
+});
+
+app.route('/upload').post(function (req, res, next) {
+    let form = new formidable.IncomingForm();
+    //Formidable uploads to operating systems tmp dir by default
+    form.uploadDir = "./uploads";       //set upload directory
+    form.keepExtensions = true;     //keep file extension
+
+    form.parse(req, function(err, fields, files) {
+        res.writeHead(200, {'content-type': 'text/plain'});
+        res.write('received upload:\n\n');
+        console.log("form.bytesReceived");
+        //TESTING
+        console.log("file size: "+JSON.stringify(files.fileUploaded.size));
+        console.log("file path: "+JSON.stringify(files.fileUploaded.path));
+        console.log("file name: "+JSON.stringify(files.fileUploaded.name));
+        console.log("file type: "+JSON.stringify(files.fileUploaded.type));
+        console.log("astModifiedDate: "+JSON.stringify(files.fileUploaded.lastModifiedDate));
+
+        //Formidable changes the name of the uploaded file
+        //Rename the file to its original name
+        fs.rename(files.fileUploaded.path, './uploads/'+files.fileUploaded.name, function(err) {
+            if (err)
+                throw err;
+            console.log('renamed complete');
+        });
+        res.end();
+    });
 });
 
 http.listen(port, function(){
